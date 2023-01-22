5ee9ab54f7ed4dff98bd8501eb1f344f0751acfc
@Viena <vbaca1977@gmail.com>
2023-01-21 22:05:25 -0600

    Agregando el mobile menu

--- frontend-developer-1/main.js
+++ frontend-developer-1/main.js
@@ -1,8 +1,15 @@
 const btnFlecha = document.querySelector ('.navbar-flechita');
-const desktopMenu = document.querySelector ('.desktop-menu')
+const desktopMenu = document.querySelector ('.desktop-menu');
+const burguerMenu = document.querySelector ('.burguer-menu');
+const mobileMenu = document.querySelector ('.mobile-menu');
 
 btnFlecha.addEventListener ('click', toggleDesktopMenu);
+burguerMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu (){
     desktopMenu.classList.toggle ('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle ('inactive');
 }
